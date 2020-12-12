b6d0fa787520a6174c471801eeaea7e481e7b570
Pawel Wozniak <rax1994@gmail.com>
2020-12-12 15:29:54 +0000

    updated index.js
    cleaned up and simplified routing (untested)

--- index.js
+++ index.js
@@ -1,73 +1,33 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-
-app.use(express.static('.'));
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const exec = require('child_process').exec;
 
 server.listen(80);
 
-const exec = require('child_process').exec;
-
+// app.use(express.static('.'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/reboot', function (req, res) {
+app.get('/reboot', (req, res) => {
   res.send('rebooting rpi')
   exec('sudo reboot');
 });
 
-app.get('/pwr', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_POWER');
-});
+app.get('/pwr', (req, res) => pressKey('POWER', res));
+app.get('/src', (req, res) => pressKey('MEDIA', res));
+app.get('/menu', (req, res) => pressKey('MENU', res));
+app.get('/mute', (req, res) => pressKey('MUTE', res));
+app.get('/up', (req, res) => pressKey('UP', res));
+app.get('/down', (req, res) => pressKey('DOWN', res));
+app.get('/left', (req, res) => pressKey('LEFT', res));
+app.get('/right', (req, res) => pressKey('RIGHT', res));
+app.get('/ok', (req, res) => pressKey('OK', res));
+app.get('/voldn', (req, res) => pressKey('VOLUMEDOWN', res));
+app.get('/volup', (req, res) => pressKey('VOLUMEUP', res));
 
-app.get('/src', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_MEDIA');
-});
-
-app.get('/menu', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_MENU');
-});
-
-app.get('/mute', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_MUTE');
-});
-
-app.get('/up', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_UP');
-});
-
-app.get('/down', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_DOWN');
-});
-
-app.get('/left', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_LEFT');
-});
-
-app.get('/right', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_RIGHT');
-});
-
-app.get('/ok', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_OK');
-});
-
-app.get('/voldn', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_VOLUMEDOWN');
-});
-
-app.get('/volup', function (req, res) {
-  res.send('OK')
-  exec('irsend send_once myremote KEY_VOLUMEUP');
-});
+function pressKey(key, res) {
+  exec(`irsend send_once myremote KEY_${key}`);
+  res.send('OK');
+}
